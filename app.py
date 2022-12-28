aa3ccbfb256a04f9150c0a5ce084d632c864830f
ShreyMehra <67088904+ShreyMehra@users.noreply.github.com>
2022-12-28 21:12:21 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import functions as f
 import pandas as pd
 import plotly.express as px
 from streamlit_option_menu import option_menu 
-
+import os, sys
 mod = {'BOT' : 'Business, Operate, Transfer', 'BOT (Toll)' : 'Business, Operate, Transfer', 'OMT' : 'Operate, Maintain and Transfer'}
 na = 'Not Available'
 
@@ -13,7 +13,7 @@ st.markdown('_SHREY MEHRA(LCO20376)_-Also, This is all ethical I am doing, just 
     
 selected = option_menu(
     menu_title = None,
-    options = ['Find based of National Highway','Find based of State'] 
+    options = ['Find based of National Highway','Find based of State', 'Find Closest Toll to a Location'] 
 )
 
 if selected == 'Find based of State':
@@ -31,80 +31,86 @@ if selected == 'Find based of State':
     st.write(f'Highway Length : {length} kms')
     st.write(f'Plaza Name : {plaza_name}')
     st.write(f'Model : {model}')
-    if len(data) > 2:
+    
+    try:
         data = pd.read_json(data).T
-        
         st.table(data)
-    else:
-        st.write('TOLL DATA AVAILABLE')
+    except:
+        st.write('_TOLL DATA NOT AVAILABLE_')
 
-    with st.expander("See Emergency Detail"):
-        st.markdown('Helpline Numbers:')
-        st.write('')
-        st.write('')
-        
-        crane, ambulance, patrol = extra['Helpline No. :'].split(',')
-        
-        crane = crane.strip().split('-')[1] if len(crane.strip().split('-')) == 2 and crane.strip().split('-')[1] != '' else na
-        ambulance = ambulance.strip().split('-')[1] if len(ambulance.strip().split('-')) == 2 and ambulance.strip().split('-')[1] != '' else na
-        patrol = patrol.strip().split('-')[1] if len(patrol.strip().split('-')) == 2 and patrol.strip().split('-')[1] != '' else na
-        
-        st.write('Crane : ', crane)
-        st.write('Ambulance : ', ambulance)
-        st.write('Route Patrol : ', patrol)
-
-        st.write('')
-        emergency = extra['Emergency Services :']
-        emergency = emergency if emergency is not None else 'Sorry! ' + na
-
-        st.markdown(f'Call for Urgency at : {patrol}')
-
-        police = extra["Nearest Police Station:"]
-        if police == '/':
-            st.write(f'Police Station  ::  {na} neither Phone Number  ::  {na}')
+    try:
+        with st.expander("See Emergency Detail"):
+            st.markdown('Helpline Numbers:')
             st.write('')
-        else:
-            if len(police.split(' / ')) == 2:
-                ps = police.split(' / ')[0]
-                num = police.split(' / ')[1]
+            st.write('')
+            
+            crane, ambulance, patrol = extra['Helpline No. :'].split(',')
+            
+            crane = crane.strip().split('-')[1] if len(crane.strip().split('-')) == 2 and crane.strip().split('-')[1] != '' else na
+            ambulance = ambulance.strip().split('-')[1] if len(ambulance.strip().split('-')) == 2 and ambulance.strip().split('-')[1] != '' else na
+            patrol = patrol.strip().split('-')[1] if len(patrol.strip().split('-')) == 2 and patrol.strip().split('-')[1] != '' else na
+            
+            st.write('Crane : ', crane)
+            st.write('Ambulance : ', ambulance)
+            st.write('Route Patrol : ', patrol)
 
-                ps, num = ps.split(','), num.split(',')
-                
-                if len(ps) == len(num):
-                    for i in range(len(ps)):
-                        st.write(f'Police Station  ::  _*{ps[i].strip()}*_ and Phone Number  ::  _*{num[i].strip()}*_')
-                        st.write('')
-                else:
-                    for i in range(min(len(ps), len(num))):
-                        st.write(f'Police Station  ::  _*{ps[i].strip()}*_ and Phone Number  ::  _*{num[i].strip()}*_')
-                        st.write('')
+            st.write('')
+            emergency = extra['Emergency Services :']
+            emergency = emergency if emergency is not None else 'Sorry! ' + na
+
+            st.markdown(f'Call for Urgency at : {patrol}')
+
+            police = extra["Nearest Police Station:"]
+            if police == '/':
+                st.write(f'Police Station  ::  {na} neither Phone Number  ::  {na}')
+                st.write('')
             else:
-                if isinstance(police.split(' / ')[0], int):
-                    num = police.split(' / ')[0]
-                    st.write(f'Police Station  ::  _*{na}*_ and Phone Number  ::  _*{num.strip()}*_')
-                    st.write('')  
-                else:
+                if len(police.split(' / ')) == 2:
                     ps = police.split(' / ')[0]
-                    st.write(f'Police Station  ::  _*{ps.strip()}*_ and Phone Number  ::  _*{na}*_')
-                    st.write('')
+                    num = police.split(' / ')[1]
 
-        
-        hospital = extra['Nearest Hospital(s):']
-        
-        if hospital != None:
-            st.markdown('_LIST OF HOSPITAL ON THE NATIONAL HIGHWAY_')
-            hospital = hospital.split(')')
-            st.write(hospital[1].rsplit(',', 1)[0].strip())
-            
-            if len(hospital) == 3:
-                st.write(hospital[2])
-            
-            if len(hospital) > 3:
-                for i in range(3, len(hospital), 2):
-                    st.write(hospital[i].rsplit(',', 1)[0].strip())            
-        else:
-            st.markdown('_No Data or Hospital Available_')
+                    ps, num = ps.split(','), num.split(',')
+                    
+                    if len(ps) == len(num):
+                        for i in range(len(ps)):
+                            st.write(f'Police Station  ::  _*{ps[i].strip()}*_ and Phone Number  ::  _*{num[i].strip()}*_')
+                            st.write('')
+                    else:
+                        for i in range(min(len(ps), len(num))):
+                            st.write(f'Police Station  ::  _*{ps[i].strip()}*_ and Phone Number  ::  _*{num[i].strip()}*_')
+                            st.write('')
+                else:
+                    if isinstance(police.split(' / ')[0], int):
+                        num = police.split(' / ')[0]
+                        st.write(f'Police Station  ::  _*{na}*_ and Phone Number  ::  _*{num.strip()}*_')
+                        st.write('')  
+                    else:
+                        ps = police.split(' / ')[0]
+                        st.write(f'Police Station  ::  _*{ps.strip()}*_ and Phone Number  ::  _*{na}*_')
+                        st.write('')
 
+            
+            hospital = extra['Nearest Hospital(s):']
+            
+            if hospital != None:
+                st.markdown('_LIST OF HOSPITAL ON THE NATIONAL HIGHWAY_')
+                try:
+                    hospital = hospital.split(')')
+                    st.write(hospital[1].rsplit(',', 1)[0].strip())
+                    
+                    if len(hospital) == 3:
+                        st.write(hospital[2])
+                    
+                    if len(hospital) > 3:
+                        for i in range(3, len(hospital), 2):
+                            st.write(hospital[i].rsplit(',', 1)[0].strip())            
+                except:
+                    st.write(hospital[0])
+                
+            else:
+                st.markdown('_No Data or Hospital Available_')
+    except:
+        st.write('_No Data or Hospital Available_')
 
     fig = px.scatter_mapbox(
         lat = [latitude], 
@@ -127,7 +133,7 @@ if selected == 'Find based of State':
     if st.button('Show on Map', key = 11):
         st.plotly_chart(fig)
     
-if selected == 'Find based of National Highway':
+elif selected == 'Find based of National Highway':
     
     highways = f.fetch_highway_wise()
     selected_highway = st.selectbox('Select State', highways)
@@ -154,14 +160,17 @@ if selected == 'Find based of National Highway':
 
             for i in range(len(length)):
                 st.write(f'Plaza Name : {plaza_name[i]}, Plaza ID : {id[i]}')
-                st.write(f'Length of Stretch : {length[i]}')
+                st.write(f'Length of Stretch : {length[i]} kms')
                 st.write(f'Model : {model[i]}')
                 st.write(f'Latitude : {latitude[i]}, Longitude : {longitude[i]}')
-                if len(details[i]) > 2:
+                
+                try:
                     data = pd.read_json(details[i]).T
                     st.table(data)
-                else:
-                    st.write('TOLL DATA AVAILABLE')
+                except:
+                    st.write('_TOLL DATA NOT AVAILABLE_')
+
+
                 try:
                     if st.button('Emergency Services', key = str(latitude[i])+plaza_name[i]+str(id[i])):    
                         st.markdown('Helpline Numbers:')
@@ -218,19 +227,23 @@ if selected == 'Find based of National Highway':
                         
                         if hospital != None:
                             st.markdown('_LIST OF HOSPITAL ON THE NATIONAL HIGHWAY_')
-                            hospital = hospital.split(')')
-                            st.write(hospital[1].rsplit(',', 1)[0].strip())
-                            
-                            if len(hospital) == 3:
-                                st.write(hospital[2])
-                            
-                            if len(hospital) > 3:
-                                for i in range(3, len(hospital), 2):
-                                    st.write(hospital[i].rsplit(',', 1)[0].strip())            
+                            try:
+                                hospital = hospital.split(')')
+                                st.write(hospital[1].rsplit(',', 1)[0].strip())
+                                
+                                if len(hospital) == 3:
+                                    st.write(hospital[2])
+                                
+                                if len(hospital) > 3:
+                                    for i in range(3, len(hospital), 2):
+                                        st.write(hospital[i].rsplit(',', 1)[0].strip())            
+                            except:
+                                st.write(hospital[0])
+                                        
                         else:
                             st.markdown('_No Data or Hospital Available_')
                 except:
-                    continue
+                    st.write('_No Data or Hospital Available_')
 
                 st.write(''); st.write(''); st.write(''); st.write(''); 
                 st.write('')
@@ -276,4 +289,106 @@ if selected == 'Find based of National Highway':
     fig.update_traces(marker={'size': 30})
     
     st.plotly_chart(fig)
-     
+
+elif selected == 'Find Closest Toll to a Location':
+    place = st.text_input('Enter the location to search for')
+    distance = st.slider('Radius of Toll Location? in kms', 0, 200, 30, 10)
+    
+    if place and distance > 0:
+        if f.fetch_location(place, distance):
+            state, highway_name, highway_length, id, plaza_name, model, details, extra = f.fetch_location(place, distance)
+
+            for i in range(len(highway_name)):
+                if model[i] in mod:
+                    model[i] = mod[model[i]]
+                
+                st.write('')
+                st.write('')
+                st.write(f'State : {state[i]}')
+                st.write(f'Highway Name : {highway_name[i]}')
+                st.write(f'Highway Length : {highway_length[i]} kms')
+                st.write(f'Plaza Name : {plaza_name[i]}')
+                st.write(f'Model : {model[i]}')
+                
+                try:
+                    data = pd.read_json(details[i]).T
+                    st.table(data)
+                except:
+                    st.write('_TOLL DATA NOT AVAILABLE_')
+
+                try:
+                    if st.button('Emergency Services', key = plaza_name[i]+str(id[i])):    
+                        st.markdown('Helpline Numbers:')
+                        st.write('')
+                        
+                        crane, ambulance, patrol = extra[i]['Helpline No. :'].split(',')
+                        
+                        crane = crane.strip().split('-')[1] if len(crane.strip().split('-')) == 2 and crane.strip().split('-')[1] != '' else na
+                        ambulance = ambulance.strip().split('-')[1] if len(ambulance.strip().split('-')) == 2 and ambulance.strip().split('-')[1] != '' else na
+                        patrol = patrol.strip().split('-')[1] if len(patrol.strip().split('-')) == 2 and patrol.strip().split('-')[1] != '' else na
+                        
+                        st.write('Crane : ', crane)
+                        st.write('Ambulance : ', ambulance)
+                        st.write('Route Patrol : ', patrol)
+
+                        st.write('')
+                        emergency = extra[i]['Emergency Services :']
+                        emergency = emergency if emergency is not None else 'Sorry! ' + na
+
+                        st.markdown(f'Call for Urgency at : {patrol}')
+
+                        police = extra[i]["Nearest Police Station:"]
+                        if police == '/':
+                            st.write(f'Police Station  ::  {na} neither Phone Number  ::  {na}')
+                            st.write('')
+                        else:
+                            if len(police.split(' / ')) == 2:
+                                ps = police.split(' / ')[0]
+                                num = police.split(' / ')[1]
+
+                                ps, num = ps.split(','), num.split(',')
+                                
+                                if len(ps) == len(num):
+                                    for i in range(len(ps)):
+                                        st.write(f'Police Station  ::  _*{ps[i].strip()}*_ and Phone Number  ::  _*{num[i].strip()}*_')
+                                        st.write('')
+                                else:
+                                    for i in range(min(len(ps), len(num))):
+                                        st.write(f'Police Station  ::  _*{ps[i].strip()}*_ and Phone Number  ::  _*{num[i].strip()}*_')
+                                        st.write('')
+                            else:
+                                if isinstance(police.split(' / ')[0], int):
+                                    num = police.split(' / ')[0]
+                                    st.write(f'Police Station  ::  _*{na}*_ and Phone Number  ::  _*{num.strip()}*_')
+                                    st.write('')  
+                                else:
+                                    ps = police.split(' / ')[0]
+                                    st.write(f'Police Station  ::  _*{ps.strip()}*_ and Phone Number  ::  _*{na}*_')
+                                    st.write('')
+
+                        
+                        hospital = extra[i]['Nearest Hospital(s):']
+                        if hospital != None:
+                            st.markdown('_LIST OF HOSPITAL ON THE NATIONAL HIGHWAY_')
+                            try:
+                                hospital = hospital.split(')')
+                                st.write(hospital[1].rsplit(',', 1)[0].strip())
+                                
+                                if len(hospital) == 3:
+                                    st.write(hospital[2])
+                                
+                                if len(hospital) > 3:
+                                    for i in range(3, len(hospital), 2):
+                                        st.write(hospital[i].rsplit(',', 1)[0].strip())            
+                            except:
+                                st.write(hospital[0])
+                        else:
+                            st.markdown('_No Data or Hospital Available_')
+                        
+                except Exception as e:
+                    st.write('_No Data or Hospital Available_')
+
+                st.write(''); st.write(''); st.write(''); st.write(''); 
+                st.write('')
+        else:
+            st.write(f'No Tolls in this radius {distance} from {place}')
--- functions.py
+++ functions.py
@@ -1,10 +1,15 @@
-import json
+import json, ast
+import geocoder
+from math import radians, cos, sin, asin, sqrt
 
 file_state = open('final_state_wise.json', 'r')
 data_state = json.load(file_state)
 
 file_highway = open('final_highway_wise.json', 'r')
 data_highway = json.load(file_highway)
+
+file_location = open('final_location_wise.json', 'r')
+data_location = json.load(file_location)
 
 extra = ["Helpline No. :", 'Emergency Services :',  "Nearest Police Station:", "Highway Administrator (Project Director):", "Project Implementation Unit(PIU)", "Regional Office(RO)", "Representative of Consultant", "Representative of Concessionaire:", "Nearest Hospital(s):"]
 
@@ -60,3 +65,53 @@ def fetch_toll_state(highway, state):
                 
     return length, id, plaza_name, latitude, longitude, model, details, extras  
 
+def haversine(lat1, lon1, lat2, lon2, radius):
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    r = 6371 
+    return c * r <= radius
+
+def fetch_loc(loc):
+    g = geocoder.bing(loc, key='At3xjpo2p2JIt2jmdhhTegFLVF6YtETmcfdn17poSPeLOof_emkySFGoZ0PUWUjm')
+    results = g.json
+    if results is not None:
+        return results['lat'], results['lng']
+    else:
+        return None
+
+def fetch_location(loc, radius):
+    coordinate = fetch_loc(loc)
+    if coordinate is None:
+        return False
+    lat, lng = coordinate
+    all_toll_lat_lng = [ast.literal_eval(a) for a in data_location.keys()]
+    list_of_close_tolls_lat_lng = [(lng1, lat1) for (lng1, lat1) in all_toll_lat_lng if lat1 != 'NOT AVAILABLE' and  lng1 != 'NOT AVAILABLE' and haversine(lat1, lng1, lat, lng, radius)]
+    
+    if len(list_of_close_tolls_lat_lng) == 0:
+        return False
+    
+    state, highway_name, highway_length, id, plaza_name, model, details, extras = [], [], [], [], [], [], [], []
+    for toll in list_of_close_tolls_lat_lng:
+        data = data_location[str(toll)] 
+        state.append(data['State_Name'])
+        name = list(data.keys())[0]
+        highway_name.append(list(data.keys())[0])
+        highway_length.append(data[list(data.keys())[0]]['Highway Length'])
+        temp_id = temp_id = list(data[list(data.keys())[0]])[-1]
+        id.append(temp_id)
+        data = data[name][temp_id]
+        plaza_name.append(data['Plaza Name'])
+        model.append(data['Model'])
+        detail = json.dumps({key:data['Details'][key] for key in data['Details'] if key not in extra})
+        help = {key:data['Details'][key] for key in data['Details'] if key in extra}
+        details.append(detail)
+        extras.append(help)
+
+    return state, highway_name, highway_length, id, plaza_name, model, details, extras
+
+
+    
